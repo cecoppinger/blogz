b8912a28042e25fc7028e08c3406b38e8a8efde8
Clarence Coppinger <thea.trio@gmail.com>
2018-10-07 22:06:03 -0500

    added templates and css

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='css')
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:num1811418@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
-  id = db.Column(db.Interger, primary_key=True)
+  id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(120))
   body = db.Column(db.Text)
 
@@ -16,6 +16,37 @@ class Blog(db.Model):
     self.title = title
     self.body = body
 
-@app.route('/blog', methods=['POST', 'GET'])
+def get_blog():
+  return Blog.query.all()
+
+@app.route('/')
+def index():
+  return redirect('/blog')
+
+@app.route('/blog')
 def blog():
-  return render_template('blog.html')
+  blog = get_blog()
+  return render_template('blog.html', blog=blog)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+  #error_msg = ''
+
+  if request.method == 'POST':
+    post_title = request.form['title']
+    post_body = request.form['body']
+
+    if not post_title or not post_body:
+      error_msg = 'Must not be empty'
+      return render_template('newpost.html', error=error_msg)
+    
+    new_post = Blog(post_title, post_body)
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect('/blog')
+
+  return render_template('newpost.html')
+
+if __name__ == '__main__':
+  app.run()
