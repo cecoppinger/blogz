e5fc332fa5b4ff06608623be6c04455d36f3c1a4
Clarence Coppinger <thea.trio@gmail.com>
2018-10-22 14:37:10 -0500

    Added pw hashing and refactored to resemble MVC

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
-from flask import Flask, request, redirect, render_template, session, flash
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:password@localhost:8889/blogz'
-app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
-app.secret_key = 'aoweifjsladkfjawoiefr123j12klj'
-
-class Blog(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(120))
-  body = db.Column(db.Text)
-  pub_date = db.Column(db.DateTime)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-  def __init__(self, title, body, user, pub_date=None):
-    self.title = title
-    self.body = body
-    if pub_date == None:
-      pub_date = datetime.utcnow()
-    self.pub_date = pub_date
-    self.user = user
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(120), nullable=False)
-  password = db.Column(db.String(120), nullable=False)
-  posts = db.relationship('Blog', backref='user', lazy=True)
-
-  def __init__(self, username, password):
-    self.username = username
-    self.password = password
+from flask import request, redirect, render_template, session, flash
+from models import User, Blog
+from app import app, db
+from hashutils import check_pw_hash, make_pw_hash
 
 def get_blog():
   return Blog.query.order_by(Blog.pub_date.desc())
@@ -102,7 +71,7 @@ def login():
     password = request.form['password']
     user = User.query.filter_by(username=username).first()
 
-    if user and user.password == password:
+    if user and check_pw_hash(password, user.pw_hash):
       session['user'] = username
       return redirect('/newpost')
     elif not username or not password:
---
+++ models.py
@@ -0,0 +1,27 @@
+from app import db
+from hashutils import make_pw_hash
+
+class Blog(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(120))
+  body = db.Column(db.Text)
+  pub_date = db.Column(db.DateTime)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+  def __init__(self, title, body, user, pub_date=None):
+    self.title = title
+    self.body = body
+    if pub_date == None:
+      pub_date = datetime.utcnow()
+    self.pub_date = pub_date
+    self.user = user
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(120), nullable=False)
+  pw_hash = db.Column(db.String(120), nullable=False)
+  posts = db.relationship('Blog', backref='user', lazy=True)
+
+  def __init__(self, username, password):
+    self.username = username
+    self.pw_hash = make_pw_hash(password)
